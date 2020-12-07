c73dc4b9996025bf153d26bc4b8c1830f2b03470
evnayd <vjane87@yahoo.com>
2020-12-07 19:10:08 +0100

    changes

--- public/index.js
+++ public/index.js
@@ -3,25 +3,11 @@ var request = new XMLHttpRequest();
 request.open("GET", requestURL);
 request.responseType = "json";
 request.send();
-
 var myTable = document.querySelector("#dataTable");
-console.log(myTable);
-var userId = document.querySelector(".id");
-console.log(userId);
-var userEmail = document.querySelector(".email");
-console.log(userEmail);
-var userName = document.querySelector(".name");
-console.log(userName);
-var userAge = document.querySelector(".age");
-var userCity = document.querySelector(".city");
-var ourTd = document.querySelector("td");
 
-//вот это работает, но не заполняет существую таблицу, а создает свои tr;
-
-/*function fillTable(data) {
+function fillTable(data) {
   data.forEach(function (object) {
     var tr = document.createElement("tr");
-    console.log(tr);
     tr.innerHTML =
       "<td>" +
       object.id +
@@ -40,22 +26,13 @@ var ourTd = document.querySelector("td");
       "</td>";
     myTable.appendChild(tr);
   });
-}*/
+}
 
-// вот это работает только для последнего объекта из users; и только если выбрать
-//var userId = document.querySelector(".id") а не  document.querySelectorAll(".id");
-
-var fillTable = function (data) {
-  data.forEach(function (object) {
-    userId.innerHTML = object.id;
-    userName.innerHTML = object.name;
-    userEmail.innerHTML = object.email;
-    userAge.innerHTML = object.age;
-    userCity.innerHTML = object.city;
-  });
-};
+function getUser(data) {
+  fillTable(data);
+}
 
 request.onload = function () {
   var users = request.response;
-  fillTable(users);
+  getUser(users);
 };
